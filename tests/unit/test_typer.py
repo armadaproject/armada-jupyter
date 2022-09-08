fb0c35c7f94ccfed39695f48553f5eacd92baa28
Sharpz7 <adam.mcarthur62@gmail.com>
2022-09-08 21:10:11 +0000

    Added unit test

--- tests/unit/test_typer.py
+++ tests/unit/test_typer.py
@@ -10,7 +10,9 @@ runner = CliRunner()
 
 
 def test_app():
-    result = runner.invoke(app, ["submit", "test.yaml"])
-    assert result.exit_code == 0
-    assert "Hello Camila" in result.stdout
-    assert "Let's have a coffee in Berlin" in result.stdout
+    file = "test.yaml"
+    result = runner.invoke(app, [file])
+
+    assert result.exit_code == 0, result.stdout
+    assert "Submitting pods from" in result.stdout, result.stdout
+    assert file in result.stdout, result.stdout
